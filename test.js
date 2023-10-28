29ca74b97ed3f064a1eb563c450b7c14ace86c00
ESJW <poolly1004@naver.com>
2023-10-28 22:42:28 +0900

    test

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ app.post('/save-to-local', (req, res) => {
         
         // 사용자가 입력한 내용을 로컬 파일로 저장합니다.
         const fs = require('fs');
-        fs.writeFileSync('myFile.txt', content);
+        fs.writeFileSync('userFile.txt', content);
 
         res.status(200).send('파일을 로컬에 성공적으로 저장했습니다.');
     } catch (error) {
